86ca06e0e3fe5f9969b21d6952d7121ef3e8a45d
Gaj Tomasz <tomasz.gaj@microsoft.wsei.edu.pl>
2020-01-26 15:39:07 +0100

    zadanie06

---
+++ js/zadanie06.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+    var shoplist = document.getElementById("shopping-list");
+
+    document.getElementById("button-1").addEventListener("click", function() {
+        var newElement = document.createElement("LI");
+        
+        shoplist.appendChild(newElement).innerHTML = "Chleb";
+        
+    });
+
+    document.getElementById("button-2").addEventListener("click", function() {
+        shoplist.removeChild(shoplist.lastElementChild);
+    });
+
+    document.getElementById("button-3").addEventListener("click", function() {
+        var clone = shoplist.children[1].cloneNode(true);
+        
+        shoplist.appendChild(clone);
+        
+    });
+});
