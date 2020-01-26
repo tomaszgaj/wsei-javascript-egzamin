422adae8e323360648b62ae3533766041f555fc7
Gaj Tomasz <tomasz.gaj@microsoft.wsei.edu.pl>
2020-01-26 15:18:35 +0100

    zadanie03

---
+++ js/zadanie03.js
@@ -0,0 +1,21 @@
+function bigestSumOfTwoElements(array) {
+
+    if(array.length == 0) {
+        return false;
+    }
+
+    else if(array.length == 1) {
+        return console.log(array[0]);
+    }
+
+    else {
+        console.log(array.sort());
+        console.log(array.reverse());
+        return console.log(array[0] + array[1]);
+    }
+}
+
+console.log(bigestSumOfTwoElements([1,2,3,4]));
+console.log(bigestSumOfTwoElements([]));
+console.log(bigestSumOfTwoElements([76]));
+console.log(bigestSumOfTwoElements([23,45,17,12]));
